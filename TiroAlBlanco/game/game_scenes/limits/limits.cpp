fc8d0c2b571a0a8f4f839d110388e0bae0fd061a
Boniich <ezequieldbo25@gmail.com>
2024-12-18 20:08:16 -0300

    feat: print correctly the game walls

--- TiroAlBlanco/game/game_scenes/limits/limits.cpp
+++ TiroAlBlanco/game/game_scenes/limits/limits.cpp
@@ -3,20 +3,45 @@
 #include "../../helpers/move_across_screen/move_across_screen.h"
 
 
-struct Limits {
+struct LineLimit {
 	int start_line;
-	int vertical_line_length;
-	int y_coord_to_top_vertical_line;
-	int y_coord_to_bottom_vertical_line;
-	//int horizontal_line_length;
-	//int y_coord_to_bottom_horizontal_line;
+	int line_length;
+	int y_coord_to_top_line;
+	int y_coord_to_bottom_line;
 };
 
-const Limits game_limits = { 2,78,3,33 };
+struct Limits {
+	LineLimit horizontal_line;
+	LineLimit vertical_line;
+};
+
+
+const LineLimit horizontal_line = { 2, 78, 3, 20 };
+const LineLimit vertical_line = { 4,20,2,77 };
+
+const Limits game_limits = { horizontal_line,vertical_line };
 
 void printLimits() {
-	for (int i = game_limits.start_line; i < game_limits.vertical_line_length; i++) {
-		moveAcrossScreen(i, game_limits.y_coord_to_top_vertical_line); printf("%c", 205);
-		moveAcrossScreen(i, game_limits.y_coord_to_bottom_vertical_line); printf("%c", 205);
+	for (int i = game_limits.horizontal_line.start_line; i < game_limits.horizontal_line.line_length; i++) {
+		moveAcrossScreen(i, game_limits.horizontal_line.y_coord_to_top_line); printf("%c", 205);
+		moveAcrossScreen(i, game_limits.horizontal_line.y_coord_to_bottom_line); printf("%c", 205);
 	}
+
+	for (int i = game_limits.vertical_line.start_line; i < game_limits.vertical_line.line_length; i++) {
+		moveAcrossScreen(game_limits.vertical_line.y_coord_to_top_line,i); printf("%c", 186);
+		moveAcrossScreen(game_limits.vertical_line.y_coord_to_bottom_line,i); printf("%c", 186);
+	}
+
+	moveAcrossScreen(game_limits.horizontal_line.start_line, game_limits.horizontal_line.y_coord_to_top_line);
+	printf("%c", 201);
+
+	moveAcrossScreen(game_limits.horizontal_line.start_line, game_limits.horizontal_line.y_coord_to_bottom_line);
+	printf("%c", 200);
+
+	moveAcrossScreen(game_limits.vertical_line.y_coord_to_bottom_line, game_limits.horizontal_line.y_coord_to_top_line);
+	printf("%c", 187);
+
+	moveAcrossScreen(game_limits.vertical_line.y_coord_to_bottom_line, game_limits.horizontal_line.y_coord_to_bottom_line);
+	printf("%c", 188);
+
 }
