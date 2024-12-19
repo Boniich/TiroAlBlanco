7b7ddde6b557d8cef0b226748465be34d4181c53
Boniich <ezequieldbo25@gmail.com>
2024-12-19 19:50:20 -0300

    feat: add static limits to the gun

--- TiroAlBlanco/game/game_scenes/player/gun/gun.cpp
+++ TiroAlBlanco/game/game_scenes/player/gun/gun.cpp
@@ -27,47 +27,50 @@ void addAtInitialPosition(const Gun gun) {
 }
 void moveToLeft(const Gun gun) {
 
-    cleanGunFromScreen(gun);
+    if (gun->x != 3) {
+        cleanGunFromScreen(gun);
 
-    gun->x--;
-    //Se puede meter las siguientes funciones en una funcion propia y hacerlas re utilizables
-    //Boca arma
-    moveAcrossScreen(gun->x, gun->y - 2); printf("%c%c", 218, 191);
-    //Cañon
-    moveAcrossScreen(gun->x, gun->y - 1); printf("%c", 179);
-    moveAcrossScreen(gun->x + 1, gun->y - 1); printf("%c", 195);
-    //Mango
-    moveAcrossScreen(gun->x + 2, gun->y - 1); printf("%c%c", 196, 191);
-    //Aparte de atras, mango y culo del arma
-    moveAcrossScreen(gun->x, gun->y); printf("%c%c%c%c", 192, 193, 196, 217);
+        gun->x--;
+        //Se puede meter las siguientes funciones en una funcion propia y hacerlas re utilizables
+        //Boca arma
+        moveAcrossScreen(gun->x, gun->y - 2); printf("%c%c", 218, 191);
+        //Cañon
+        moveAcrossScreen(gun->x, gun->y - 1); printf("%c", 179);
+        moveAcrossScreen(gun->x + 1, gun->y - 1); printf("%c", 195);
+        //Mango
+        moveAcrossScreen(gun->x + 2, gun->y - 1); printf("%c%c", 196, 191);
+        //Aparte de atras, mango y culo del arma
+        moveAcrossScreen(gun->x, gun->y); printf("%c%c%c%c", 192, 193, 196, 217);
+    }
 
 }
 void moveToRight(const Gun gun) {
 
-    cleanGunFromScreen(gun);
-    gun->x++;
-    //Boca arma
-    moveAcrossScreen(gun->x, gun->y - 2); printf("%c%c", 218, 191);
-    //Cañon
-    moveAcrossScreen(gun->x, gun->y - 1); printf("%c", 179);
-    moveAcrossScreen(gun->x + 1, gun->y - 1); printf("%c", 195);
-    //Mango
-    moveAcrossScreen(gun->x + 2, gun->y - 1); printf("%c%c", 196, 191);
-    //Aparte de atras, mango y culo del arma
-    moveAcrossScreen(gun->x, gun->y); printf("%c%c%c%c", 192, 193, 196, 217);
+    if (gun->x != 73) {
+        cleanGunFromScreen(gun);
+        gun->x++;
+        //Boca arma
+        moveAcrossScreen(gun->x, gun->y - 2); printf("%c%c", 218, 191);
+        //Cañon
+        moveAcrossScreen(gun->x, gun->y - 1); printf("%c", 179);
+        moveAcrossScreen(gun->x + 1, gun->y - 1); printf("%c", 195);
+        //Mango
+        moveAcrossScreen(gun->x + 2, gun->y - 1); printf("%c%c", 196, 191);
+        //Aparte de atras, mango y culo del arma
+        moveAcrossScreen(gun->x, gun->y); printf("%c%c%c%c", 192, 193, 196, 217);
+    }
+
 }
 
 void cleanGunFromScreen(const Gun gun) {
-    //Boca arma
-    moveAcrossScreen(gun->x, gun->y - 2); printf("   ");
-    //Cañon
-    moveAcrossScreen(gun->x, gun->y - 1); printf("   ");
-    moveAcrossScreen(gun->x + 1, gun->y - 1); printf("   ");
-    //Mango
-    moveAcrossScreen(gun->x + 2, gun->y - 1); printf("  ");
-                                              //printf("%c%c");
-    //Aparte de atras, mango y culo del arma
-  
-    moveAcrossScreen(gun->x, gun->y); printf("    ");
-                                      //printf("%c%c%c%c");
+        //Boca arma
+        moveAcrossScreen(gun->x, gun->y - 2); printf("   ");
+        //Cañon
+        moveAcrossScreen(gun->x, gun->y - 1); printf("   ");
+        moveAcrossScreen(gun->x + 1, gun->y - 1); printf("   ");
+        //Mango
+        moveAcrossScreen(gun->x + 2, gun->y - 1); printf("  ");
+        //Aparte de atras, mango y culo del arma
+
+        moveAcrossScreen(gun->x, gun->y); printf("    ");
 }
