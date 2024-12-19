2d40e83153790e682cb97c58cf91e5a993411542
Boniich <ezequieldbo25@gmail.com>
2024-12-19 18:38:45 -0300

    no changes

--- TiroAlBlanco/TiroAlBlanco.cpp
+++ TiroAlBlanco/TiroAlBlanco.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "game/helpers/move_across_screen/move_across_screen.h"
 #include "game/helpers/hide_cursor/hide_cursor.h"
+#include "game/game_scenes/limits/limits.h"
 
 /*
  First version of game Tiro al blanco
@@ -11,9 +12,14 @@ int main()
 {
     hideCursor();
 
-    moveAcrossScreen(50, 50);
-    printf("*");
  
-
-    system("PAUSE");
+    printLimits();
+    //moveAcrossScreen(50, 50);
+    //printf("*");
+ 
+ 
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "" << std::endl;
 }
